12319b0434b34edb0515a984ba16cdda453a4939
peterfgeorge <petegeo52@gmail.com>
2025-01-25 00:54:57 -0500

    local multiplayer kinda working

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed = 5f;
+    private Vector2 moveDirection;
+
+    public InputActionReference move;
+    public InputActionReference fire;
+
+    private void Update()
+    {
+        moveDirection = move.action.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+
+    private void OnEnable()
+    {
+        fire.action.started += Fire;
+    }
+
+    private void OnDisable()
+    {
+        fire.action.started -= Fire;
+    }
+
+    private void Fire(InputAction.CallbackContext obj)
+    {
+        Debug.Log("Fired!");
+    }
+}
